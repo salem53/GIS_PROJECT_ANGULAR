5b5bb746018e0836aa03767cd755b963e850002a
salem53 <salem.dhouimir@ensi-uma.tn>
2021-10-19 20:33:09 +0100

    gis project initialisation

---
+++ src/main/java/com/hin/spatial/postgis/model/User.java
@@ -0,0 +1,30 @@
+package com.hin.spatial.postgis.model;
+
+import com.vividsolutions.jts.geom.Point;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "user")
+@Table(name ="user",uniqueConstraints={@UniqueConstraint(columnNames={"email"})})
+public class User {
+
+	@Id     @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	private long id;
+	
+	@Column(name="firstName")
+	private String firstName;
+
+	@Column(name="lastName")
+	private String lastName;
+
+
+	@Column(name="email")
+	private String email;
+
+	@Column(name="password")
+	private String password;
+
+}
