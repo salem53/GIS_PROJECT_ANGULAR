06b6a7ddedcebc46b9e68768947d40ac56cf6466
khawla <khawla.ezzina@ensi-uma.tn>
2021-10-24 17:21:59 +0100

    Khawla first commit

--- src/main/java/com/hin/spatial/postgis/model/User.java
+++ src/main/java/com/hin/spatial/postgis/model/User.java
@@ -1,30 +1,58 @@
 package com.hin.spatial.postgis.model;
 
+import com.sun.istack.NotNull;
 import com.vividsolutions.jts.geom.Point;
 import lombok.Data;
 
 import javax.persistence.*;
 
+
 @Data
-@Entity(name = "user")
-@Table(name ="user",uniqueConstraints={@UniqueConstraint(columnNames={"email"})})
+@Entity(name = "utilisateur")
+@Table(name ="utilisateur",uniqueConstraints={@UniqueConstraint(columnNames={"email"})})
 public class User {
 
-	@Id     @GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="id")
 	private long id;
 	
 	@Column(name="firstName")
+	@NotNull
 	private String firstName;
 
 	@Column(name="lastName")
+	@NotNull
 	private String lastName;
 
 
 	@Column(name="email")
+	@NotNull
 	private String email;
 
 	@Column(name="password")
+	@NotNull
 	private String password;
 
+	public User(long id, String firstName, String lastName, String email, String password) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public User() {
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", email='" + email + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
 }
