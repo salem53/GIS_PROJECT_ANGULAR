6753dd63734085a69a7a1bd9bb6c0cc958de2140
salem53 <salem.dhouimir@ensi-uma.tn>
2021-11-07 17:41:30 +0100

    update trajet controller

--- src/main/java/com/hin/spatial/postgis/controller/userController.java
+++ src/main/java/com/hin/spatial/postgis/controller/userController.java
@@ -1,6 +1,7 @@
 package com.hin.spatial.postgis.controller;
 
 import com.hin.spatial.postgis.model.User;
+import com.hin.spatial.postgis.repo.userRepository;
 import com.hin.spatial.postgis.service.userServiceImpl;
 import com.hin.spatial.postgis.controller.encryptDecrypt.AES;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -15,11 +17,18 @@ import javax.validation.Valid;
 public class userController {
     @Autowired
     private userServiceImpl userService;
+    @Autowired
+    private userRepository userRepository;
 
     @GetMapping("/{userId}")
     public java.util.Optional<User> getUser(@PathVariable Long userId) {
 
         return  userService.findById(userId);
+    }
+    @GetMapping("/all")
+    public List<User> getAllUser() {
+
+        return  userRepository.findAll();
     }
     @PostMapping("/add")
     public User createUser(@Valid @RequestBody User admin) {
